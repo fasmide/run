6067e98aefda9dcdb217fa5ed3bccc1de35ae70a
Kristian Mide <fas@mide.dk>
2016-06-02 23:23:20 +0200

    New start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@
 raspi-gpio set 17 pu
 raspi-gpio set 27 pu
 raspi-gpio set 22 pu
+xset -dpms
+xset s off
+matchbox-window-manager -use_titlebar no -use_cursor no &
 sleep 15
 iceweasel http://localhost:8080 &
 sleep 15
